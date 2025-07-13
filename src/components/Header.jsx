0357c47ea3f99049d5b6ae2022088cea98501a63
gyongcode <gyongcode@gmail.com>
2025-07-13 23:22:31 +0900

    메인 페이지 url 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,46 @@
-'use client';
+"use client";
 
-import styles from './header.css';
+import styles from "./header.css";
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Header() {
-    // TODO : 나중에 실제 데이터로 변경
-    const userName = '이정아';
-    // ----
+  // TODO : 나중에 실제 데이터로 변경
+  const userName = "이정아";
+  // ----
 
-    const router = useRouter();
-    const handleLogout = () => {
-        // ✅ 세션/스토리지 클리어
-        localStorage.clear();
-        sessionStorage.clear();
+  const router = useRouter();
+  const handleLogout = () => {
+    // ✅ 세션/스토리지 클리어
+    localStorage.clear();
+    sessionStorage.clear();
 
-        // 로그인 이동
-        router.replace('/login');
-    };
+    // 로그인 이동
+    router.replace("/login");
+  };
 
-    return (
-        <header className='header-container'>
-            {/* 로고 */}
-            <img src={`/images/common/playdata.svg`} alt='logo' className='logo' />
+  return (
+    <header className="header-container">
+      {/* 로고 */}
+      <img src={`/images/common/playdata.svg`} alt="logo" className="logo" />
 
-            {/* 메뉴바 - 본인 페이지 경로로 바꿔주세요 */}
-            <nav className='menu-bar'>
-                <Link href='/posts'>커뮤니티</Link>
-                <Link href='/consulting'>예약</Link>
-                <Link href='/news'>뉴스</Link>
-            </nav>
+      {/* 메뉴바 - 본인 페이지 경로로 바꿔주세요 */}
+      <nav className="menu-bar">
+        <Link href="/suggestion">커뮤니티</Link>
+        <Link href="/consulting">예약</Link>
+        <Link href="/news">뉴스</Link>
+      </nav>
 
-            {/* 사용자 정보 */}
-            <div className='user-info'>
-                <img src={`/images/common/user.png`} alt='user icon' className='icon' />
-                <span className='username'>{userName}</span>
-                <span className='logout' onClick={handleLogout}>
-                    Logout
-                </span>
-            </div>
-        </header>
-    );
+      {/* 사용자 정보 */}
+      <div className="user-info">
+        <img src={`/images/common/user.png`} alt="user icon" className="icon" />
+        <span className="username">{userName}</span>
+        <span className="logout" onClick={handleLogout}>
+          Logout
+        </span>
+      </div>
+    </header>
+  );
 }
